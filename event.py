4f08587c213c3329efc1a743ea2bc76920c38ea1
salisbury-jaustin <salisbury.dvlpr@gmail.com>
2020-03-10 19:41:18 -0500

    added functionality to create multiple events based on event date

--- event.py
+++ event.py
@@ -1,6 +1,29 @@
 # creates an event in a users google calendar using google calendar api
 # documentation at https://developers.google.com/calendar/quickstart/python
 
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import quickstart
+import re
+import pprint
+
+#while True:
+#    quickstart.main()
+#    break
+#
+## call calendar api
+#now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+#events_result = service.events().list(calendarId='primary', timeMin=now,
+#                                        maxResults=10, singleEvents=True,
+#                                        orderBy='startTime').execute()
+#events = events_result.get('items', [])
+
+
 # query returns user input as eventList
 def query():
     print('Event:')
@@ -12,23 +35,26 @@ def query():
     print('description:')
     description = input()
 
-    print('date (yyyy-mm-dd):')
-    date = input()
+    print('wedding date (yyyy-mm-dd):')
+    weddingDate = input()
 
     print('start time:')
     timeStart = 'T' + input() + ':00-06:00'
 
-    dateTimeStart = date + timeStart
+    dateTimeStart = weddingDate + timeStart
     
     print ('end time:')
     timeEnd = 'T' + input() + ':00-06:00'
     
-    dateTimeEnd = date + timeEnd
+    dateTimeEnd = weddingDate + timeEnd
 
     print('email of coordinator:')
     email = input()
 
-    eventList = [summary, location, description, dateTimeStart, dateTimeEnd, email]
+    print('book date:')
+    bookDate = input()
+
+    eventList = [summary, location, description, dateTimeStart, dateTimeEnd, email, weddingDate, bookDate]
     return eventList
 
 # add event to calendar
@@ -58,7 +84,79 @@ def createEvent():
                 },
             }
 
-    event = service.events().insert(calendarId='primary', body=event().execute())
-    print('Event created: %s' % (event.get('htmlLink')))
+    # use of dateRegex allows for grouping date strings 'yyyy-mm-dd' into year, month, and day groups
+    dateRegex = re.compile(r'(\d{4})-(\d{2})-(\d{2})')
+
+    # creates weddingDate and bookDate variables of type <class 'datetime.date'> to allow for date.timedelta() calculations
+    moWeddingDate = dateRegex.search(eventList[6])
+    weddingDateIntList = [int(i) for i in moWeddingDate.groups()]
+    year, month, day = weddingDateIntList
+    weddingDate = datetime.date(year, month, day)
+
+    moBookDate = dateRegex.search(eventList[7])
+    bookDateIntList = [int(i) for i in moBookDate.groups()]
+    year, month, day = bookDateIntList
+    bookDate = datetime.date(year, month, day)
+    
+    # calculates a list of important dates
+    importantDates = [
+            ((bookDate + (weddingDate - bookDate)/2) - datetime.timedelta(weeks=2)),
+            (bookDate + (weddingDate - bookDate)/2),
+            (weddingDate - datetime.timedelta(days=75)),
+            (weddingDate - datetime.timedelta(days=44)),
+            (weddingDate - datetime.timedelta(weeks=4)),
+            (weddingDate - datetime.timedelta(weeks=3))
+            ]
+    # converts importantDates datetime.date objects to strings using dateTimeObj.strftime()
+    importantDates = [i.strftime('%Y-%m-%d') for i in importantDates]
+
+    # list of reminders for important dates
+    twoWeeksTilHalfway = 'second payment reminder for' + ' ' + eventList[0]
+    halfWay = 'second payment due for' + ' ' + eventList[0]
+    seventyFiveDays = 'schedule design meeting for' + ' ' + eventList[0]
+    fortyFourDays = 'send final payment reminder for' + ' ' + eventList[0]
+    oneMonth = 'final payment due for' + ' ' + eventList[0] + ' ' + '(order flowers)'
+    threeWeeks = 'send questionnaire for' + ' ' + eventList[0]
+    reminders = [
+            twoWeeksTilHalfway,
+            halfWay,
+            seventyFiveDays,
+            fortyFourDays,
+            oneMonth,
+            threeWeeks
+            ]
+    # create a dict reminderDate{} with items from importantDates[] and reminders[]
+    reminderDate = {}
+    for i, x in zip(importantDates, reminders):
+        reminderDate.setdefault(i, x)
+
+    # create events for k, v in reminderDate.items() and pass to service.events() from google calender api
+    # each event acts as a reminder for certain dates leading up to the event/wedding
+    for k, v in reminderDate.items():
+            event = {
+            'summary': v,
+            'location': '',  
+            'description': '',  
+            'start': {
+                'dateTime': k,
+                'timeZone': 'America/Chicago',
+                },
+            'end': {
+                'dateTime': '',  
+                'timeZone': 'America/Chicago',
+                },
+            'recurrence': [],
+            'attendees': [
+                {'email': '' },
+                ],
+            'reminders': {
+                'useDefault': False,
+                'overrides': [],
+                },
+            }      
+            pprint.pprint(event)
+            
+            #event = service.events().insert(calendarId='primary', body=event().execute())
+            #print('Event created: %s' % (event.get('htmlLink')))
 
 createEvent()
